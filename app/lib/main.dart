1d700edb7c01b466aa58f1352d70db2688aded1f
negito6 <negito6@users.noreply.github.com>
2023-08-05 20:33:20 +0900

    map sample works

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -86,34 +88,32 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.509364, -0.128928),
+          zoom: 9.2,
+          interactiveFlags: InteractiveFlag.all,
+          enableScrollWheel: true,
+          scrollWheelVelocity: 0.00001,
         ),
+        layers: [
+          //背景地図読み込み (OSM)
+          TileLayerOptions(
+            urlTemplate: "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
+          ),
+          // サークルマーカー設定
+          CircleLayerOptions(
+            circles: [
+              CircleMarker(
+                color: Colors.indigo.withOpacity(0.9),
+                radius: 10,
+                borderColor: Colors.white.withOpacity(0.9),
+                borderStrokeWidth: 3,
+                point: LatLng(35.681, 139.760),
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
